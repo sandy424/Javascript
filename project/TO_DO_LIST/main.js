fb025815a31497bbc0125db3e8f806b199ebb42d
sandy424 <sandy424@naver.com>
2025-03-21 13:32:39 +0900

    [update] js기능 추가

--- project/TO_DO_LIST/main.js
+++ project/TO_DO_LIST/main.js
@@ -1,13 +1,25 @@
-const inputBox = document.getElementById('input-box');
-const checkList = document.getElementById('list');
+const inputBox = document.getElementById("input-box");
+const checkList = document.getElementById("list");
 
 // 추가 버튼 함수
 function addTask() {
-    if(inputBox === '') {
+    if(inputBox.value === '') {
         alert('입력된 내용이 없습니다.');
     }else {
-        let li = document.createElement('li');
+        let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         checkList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-}
+    inputBox.value = "";
+} 
+
+checkList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
